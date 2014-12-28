3195d8ef6e743c3516ac86032725387d1f2bf797
Mian Uddin <mianuddin@gmail.com>
2014-12-27 23:41:43 -0800

    Added pipes and bird (LilB).

---
+++ core/src/com/mianuddin/flappyB/components/LilB.java
@@ -0,0 +1,7 @@
+package com.mianuddin.flappyB.components;
+
+/**
+ * Created by Mian on 12/27/2014.
+ */
+public class LilB {
+}
---
+++ core/src/com/mianuddin/flappyB/components/Pipes.java
@@ -0,0 +1,4 @@
+package com.mianuddin.flappyB.components;
+
+public class Pipes {
+}
