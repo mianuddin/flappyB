bdbd671d199571fa1dbee3846b540a94f01a4584
Mian Uddin <mianuddin@gmail.com>
2014-12-27 22:55:59 -0800

    Initial commit

---
+++ android/src/com/mianuddin/flappyB/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mianuddin.flappyB.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mianuddin.flappyB.flappyB;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new flappyB(), config);
+	}
+}
